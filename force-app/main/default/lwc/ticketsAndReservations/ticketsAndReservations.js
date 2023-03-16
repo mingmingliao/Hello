acedfe74611da98ff32fcb9a443693a919b12eba
mingmingliao <mingmingliao98@gmail.com>
2023-03-16 19:37:34 -0400

    Initial commit

---
+++ force-app/main/default/lwc/ticketsAndReservations/ticketsAndReservations.js
@@ -0,0 +1,16 @@
+import { LightningElement } from 'lwc';
+import CreateTripModal from 'c/createTripModal';
+
+export default class TicketsAndReservations extends LightningElement {
+    handleClick() {
+        CreateTripModal.open({
+          // maps to developer-created `@api options`
+          options: [
+            { id: 1, label: 'Option 1' },
+            { id: 2, label: 'Option 2' },
+          ]
+        }).then((result) => {
+            console.log(result);
+        });
+    }
+}
