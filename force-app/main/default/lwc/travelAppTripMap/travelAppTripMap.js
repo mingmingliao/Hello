a96c79c713bef4657e50cf55f35da79683983296
EDWARDMAI <edwardmai86@gmail.com>
2023-03-23 18:58:37 -0400

    Added a mock markers to our map

--- force-app/main/default/lwc/travelAppTripMap/travelAppTripMap.js
+++ force-app/main/default/lwc/travelAppTripMap/travelAppTripMap.js
@@ -1,3 +1,27 @@
 import { LightningElement, api  } from 'lwc';
 
-export default class TravelAppTripMap extends LightningElement {}
+export default class TravelAppTripMap extends LightningElement {
+    
+    // mock address atm
+    mapMarkers = [
+        {
+            location: {
+                Street: '1 Market St',
+                City: 'San Francisco',
+                Country: 'USA',
+            },
+            title: 'The Landmark Building',
+            description:
+                'Historic <b>11-story</b> building completed in <i>1916</i>',
+        },
+    ];
+    // mock
+    zoomLevel = 11
+    center = {
+        location: {
+            State: 'San Francisco'
+        }
+    }
+
+
+}
