538702fb39273715edc3534b5b313f70072d578c
EDWARDMAI <edwardmai86@gmail.com>
2023-03-23 19:29:54 -0400

    Updated bad layout and naming

--- force-app/main/default/lwc/restaurantModal/restaurantModal.js
+++ force-app/main/default/lwc/restaurantModal/restaurantModal.js
@@ -1,5 +1,6 @@
 import { api } from 'lwc';
 import LightningModal from 'lightning/modal';
+
 import NAME_FIELD from '@salesforce/schema/Restaurant__c.Name';
 import RESTAURANT_OBJECT from '@salesforce/schema/Restaurant__c';
 import ADDRESS_FIELD from '@salesforce/schema/Restaurant__c.Address__c';
@@ -7,6 +8,8 @@ import DESCRIPTION_FIELD from '@salesforce/schema/Restaurant__c.Description__c';
 
 export default class restaurantModal extends LightningModal {
     @api content;
+
+    // TODO Pull this in later from code
     objectApiName = "Restaurant__c"
 
     // Expose a field to make it available in the template
