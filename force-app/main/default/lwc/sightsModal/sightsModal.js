50d5b8c1edf5f6f4a5ec1ee52de7f44b76483f44
mingmingliao <mingmingliao98@gmail.com>
2023-03-28 16:28:07 -0400

    Sights Modal small changes

--- force-app/main/default/lwc/sightsModal/sightsModal.js
+++ force-app/main/default/lwc/sightsModal/sightsModal.js
@@ -1,8 +1,23 @@
 import { api } from 'lwc';
 import LightningModal from 'lightning/modal';
+import SIGHT_OBJECT from '@salesforce/schema/Sight__c';
+import NAME_FIELD from '@salesforce/schema/Sight__c.Name';
+import ADDRESS_FIELD from '@salesforce/schema/Sight__c.Address__c';
+import DESCRIPTION_FIELD from '@salesforce/schema/Sight__c.Description__c';
+
 
 export default class sightsModal extends LightningModal {
     @api content;
+
+    // TODO Pull this in later from code
+    objectApiName = "Sight__c"
+
+    // Expose a field to make it available in the template
+    fields = [NAME_FIELD, DESCRIPTION_FIELD, ADDRESS_FIELD];
+    
+    // Flexipage provides recordId and objectApiName
+     @api recordId;
+     @api objectApiName = RESTAURANT_OBJECT;
 
     handleOkay() {
         this.close('okay');
